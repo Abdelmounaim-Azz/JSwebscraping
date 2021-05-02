f99194241d22dae77dae84018c52d1880c569dc9
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-02 11:29:10 +0000

    github jobs fetch

--- project1/index.js
+++ project1/index.js
@@ -1,6 +1,9 @@
 const Sheet = require("./utils/Sheet");
+const fetch = require("node-fetch");
 (async function () {
   const sheet = new Sheet();
   await sheet.load();
   await sheet.addRows([{title: "hooray", location: "nowhere"}]);
+  const jobs = fetch("https://jobs.github.com/positions.json");
+  console.log(jobs[0]);
 })();
