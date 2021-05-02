be19e88e5c64750f85784d7b0141f44a662c595c
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-02 10:48:26 +0000

    add class Sheet

--- project1/index.js
+++ project1/index.js
@@ -1,20 +1,6 @@
-const {GoogleSpreadsheet} = require("google-spreadsheet");
-require("dotenv").config();
-
-// Initialize the sheet - doc ID is the long id in the sheets URL
-const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_URL);
-
-(async function changeTitle() {
-  await doc.useServiceAccountAuth({
-    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
-  });
-
-  await doc.loadInfo(); // loads document properties and worksheets
-  console.log(doc.title);
-  await doc.updateProperties({title: "renamed document"});
-  const sheet = doc.sheetsByIndex[0];
-  const moreRows = await sheet.addRows([
-    {title: "hooray", location: "nowhere"},
-  ]);
+const Sheet = require("./utils/Sheet");
+(async function () {
+  const sheet = new Sheet();
+  await sheet.load();
+  await sheet.addRows([{title: "hooray", location: "nowhere"}]);
 })();
