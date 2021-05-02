c56c35dff3fdbdc0fe42bd76b9c28779be00d70f
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-02 14:30:06 +0000

    map result and save to db

--- project1/index.js
+++ project1/index.js
@@ -1,10 +1,20 @@
 const Sheet = require("./utils/Sheet");
 const fetch = require("node-fetch");
 (async function () {
+  const jobs = await fetch(
+    "https://jobs.github.com/positions.json?location=remote"
+  );
+  const res = await jobs.json();
+  const rows = res.map((job) => {
+    return {
+      companyName: job.company,
+      companyWeb: job.company_url,
+      title: job.title,
+      location: job.location,
+      createdAt: job.created_at,
+    };
+  });
   const sheet = new Sheet();
   await sheet.load();
-  await sheet.addRows([{title: "added", location: "now"}]);
-  const jobs = await fetch("https://jobs.github.com/positions.json");
-  const res = await jobs.json();
-  console.log(res);
+  await sheet.addRows(rows);
 })();
