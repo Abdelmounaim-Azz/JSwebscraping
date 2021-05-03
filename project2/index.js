9f26ceddd85e94e9c317f0f4aa4a82bfe930f731
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-03 10:53:32 +0000

    twitter getRows from googleSheet

--- project2/index.js
+++ project2/index.js
@@ -1,18 +1,15 @@
 require("dotenv").config();
+const Sheet = require("./utils/Sheet");
 const Twitter = require("twitter");
-let client = new Twitter({
-  consumer_key: process.env.API_KEY,
-  consumer_secret: process.env.API_SECRET_KEY,
-  access_token_key: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-});
-
-client.post(
-  "statuses/update",
-  {status: "I Love Twitter"},
-  function (error, tweet, response) {
-    if (error) throw error;
-    console.log(tweet); // Tweet body.
-    console.log(response); // Raw response object.
-  }
-);
+(async function () {
+  const client = new Twitter({
+    consumer_key: process.env.API_KEY,
+    consumer_secret: process.env.API_SECRET_KEY,
+    access_token_key: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  });
+  const sheet = new Sheet();
+  await sheet.load();
+  const rows = await sheet.getRows();
+  console.log(rows);
+})();
---
+++ project2/utils/Sheet.js
@@ -0,0 +1,23 @@
+const {GoogleSpreadsheet} = require("google-spreadsheet");
+require("dotenv").config();
+module.exports = class Sheet {
+  constructor() {
+    this.doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_URL);
+  }
+  async load() {
+    await this.doc.useServiceAccountAuth({
+      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY,
+    });
+
+    await this.doc.loadInfo();
+  }
+  async addRows(rows) {
+    const sheet = this.doc.sheetsByIndex[0];
+    await sheet.addRows(rows);
+  }
+  async getRows() {
+    const sheet = this.doc.sheetsByIndex[0];
+    return await sheet.getRows();
+  }
+};
