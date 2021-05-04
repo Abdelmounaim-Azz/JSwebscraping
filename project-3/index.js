6028de485fd4a723293e00a7e9c281cc404172fd
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-04 14:27:16 +0000

    cheerio parse Title description and seaches per month

--- project-3/index.js
+++ project-3/index.js
@@ -5,6 +5,17 @@ const cheerio = require("cheerio");
   const data = await fetch("https://explodingtopics.com/topics-this-month");
   const text = await data.text();
   const $ = cheerio.load(text);
-  const topicTitle = $(".tileKeyword").text();
-  console.log(topicTitle);
+  const containers = $(".topicInfoContainer").toArray();
+  const topics = containers.map((trend) => {
+    const trendDoc = $(trend);
+    const title = trendDoc.find(".tileKeyword").text();
+    const desc = trendDoc.find(".tileDescription").text();
+    const SearchMonth = trendDoc.find("scoreTag").first().text();
+    return {
+      title,
+      desc,
+      SearchMonth,
+    };
+  });
+  console.log(topics);
 })();
