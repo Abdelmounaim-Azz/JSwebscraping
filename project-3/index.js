334175487855250089bacd9a79b4a754a41d177e
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-04 12:51:45 +0000

    fetch check

--- project-3/index.js
+++
@@ -1,8 +0,0 @@
-const fetch = require("node-fetch");
-
-(async function () {
-  const data = await fetch("https://explodingtopics.com/topics-this-month");
-  const res = await data.text();
-  const found = res.includes("google classroom");
-  console.log(res);
-})();
