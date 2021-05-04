ce2285d2025a6907bea5a239920e045a63a63646
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-04 11:32:56 +0000

    fetch hacker news website

--- project-3/index.js
+++ project-3/index.js
@@ -2,6 +2,5 @@ const fetch = require("node-fetch");
 
 (async function () {
   const data = await fetch("https://news.ycombinator.com/");
-  const res = data.json();
-  console.log(res);
+  console.log(data);
 })();
