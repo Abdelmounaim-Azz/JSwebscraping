2355e98a4c0db9757a328b08bf5fd1461e7e6f90
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-04 11:39:06 +0000

    sanity check for the markup

--- project-3/index.js
+++
@@ -1,7 +0,0 @@
-const fetch = require("node-fetch");
-
-(async function () {
-  const data = await fetch("https://news.ycombinator.com/");
-  const res = await data.text();
-  console.log({res});
-})();
