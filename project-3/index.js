732f42a131d52a6582c52187764a3dedfc0b8cc7
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-04 11:43:23 +0000

    try fetching amazon

--- project-3/index.js
+++ project-3/index.js
@@ -1,8 +1,10 @@
 const fetch = require("node-fetch");
 
 (async function () {
-  const data = await fetch("https://news.ycombinator.com/");
+  const data = await fetch(
+    "https://www.amazon.com/TCL-4K-Smart-LED-50S435/dp/B08DHFX4FV/"
+  );
   const res = await data.text();
-  const found = res.includes("Excalidraw");
+  const found = res.includes("TCL");
   console.log(found);
 })();
