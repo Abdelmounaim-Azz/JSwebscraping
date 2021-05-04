dfb7ab549a4c7f4b0c4de17b769693dcc9542325
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-04 15:27:07 +0000

    use htmlparser2 to parse doc and pass the result to cheerio load

--- project-3/index.js
+++ project-3/index.js
@@ -1,10 +1,18 @@
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
+const htmlparser2 = require("htmlparser2");
 
 (async function () {
   const data = await fetch("https://explodingtopics.com/topics-this-month");
+  const options = {
+    xmlMode: true,
+    decodeEntities: true, // Decode HTML entities.
+    withStartIndices: false, // Add a `startIndex` property to nodes.
+    withEndIndices: false, // Add an `endIndex` property to nodes.
+  };
   const text = await data.text();
-  const $ = cheerio.load(text, {
+  const dom = htmlparser2.parseDOM(text, options);
+  const $ = cheerio.load(dom, {
     xml: {
       normalizeWhitespace: true,
     },
