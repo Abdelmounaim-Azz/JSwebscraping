207d25f30d168677c6d3223808efb64163e230d6
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-04 12:47:42 +0000

    fetch exploding topic website

--- project-3/index.js
+++ project-3/index.js
@@ -1,10 +1,8 @@
 const fetch = require("node-fetch");
 
 (async function () {
-  const data = await fetch(
-    "https://www.amazon.com/TCL-4K-Smart-LED-50S435/dp/B08DHFX4FV/"
-  );
+  const data = await fetch("https://explodingtopics.com/topics-this-month");
   const res = await data.text();
-  const found = res.includes("TCL");
-  console.log(found);
+  const found = res.includes("google classroom");
+  console.log(res);
 })();
