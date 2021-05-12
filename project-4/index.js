c5d0d9ad7c8ea61e8e46e6f1f77e359bebbf67d0
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-12 23:19:54 +0000

    sanity check on scrapping

--- project-4/index.js
+++ project-4/index.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 
 (async function () {
-  const res = fetch("https://www.investing.com/equities/facebook-inc");
+  const res = await fetch("https://www.investing.com/equities/facebook-inc");
   const text = await res.text();
   const found = text.includes("302.55");
   console.log({found});
