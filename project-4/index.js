2778aa1a83d34fc3bd193db134983a7be1a18a3d
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-12 23:53:04 +0000

    att select price

--- project-4/index.js
+++ project-4/index.js
@@ -4,6 +4,7 @@ const cheerio = require("cheerio");
 (async function () {
   const res = await fetch("https://www.investing.com/equities/facebook-inc");
   const text = await res.text();
-  const found = text.includes("302.55");
-  console.log({found});
+  const $ = cheerio.load(text);
+  const price = $("[data-test=instrument-price-last]").first().text();
+  console.log(price);
 })();
