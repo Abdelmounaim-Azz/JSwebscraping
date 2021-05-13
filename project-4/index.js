869b3d4b729447ef1cd9ee0dea34b6a04189dc87
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-13 03:02:05 +0000

    save stockPrices to google spreadsheet

--- project-4/index.js
+++ project-4/index.js
@@ -1,10 +1,23 @@
 const fetch = require("node-fetch");
 const cheerio = require("cheerio");
-
+const Sheet = require("./utils/Sheet");
 (async function () {
-  const res = await fetch("https://www.investing.com/equities/google-inc");
+  const sheet = new Sheet();
+  await sheet.load();
+  const stocks = await sheet.getRows(0);
+  let tickerPrices = {};
+  for (let stock of stocks) {
+    const price = await scrapStockPrice(stock.url);
+    tickerPrices[stock.ticker] = price;
+  }
+  tickerPrices.date = new Date().toDateString();
+  await sheet.addRows([tickerPrices], 1);
+})();
+
+async function scrapStockPrice(url) {
+  const res = await fetch(url);
   const text = await res.text();
   const $ = cheerio.load(text);
   const price = $("[data-test=instrument-price-last]").first().text();
-  console.log(price);
-})();
+  return price;
+}
