82012c1cb2b3f6f6fc699763a13c23716f711279
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-14 08:49:41 +0000

    reddit expand btns

--- project-5/index.js
+++ project-5/index.js
@@ -1,11 +1,16 @@
 const puppeteer = require("puppeteer");
-
+const url =
+  "https://old.reddit.com/r/learnprogramming/comments/4q6tae/i_highly_recommend_harvards_free_online_2016_cs50/";
 (async function () {
   //boiler plate
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  await page.goto("https://example.com");
-  await page.screenshot({path: "example.png"});
-
-  await browser.close();
+  await page.goto(url);
+  //expand all comment threads
+  const expandsBtns = await page.$$(".morecomments");
+  console.log(expandsBtns);
+  //select all cmnts and scrape text+point
+  //sort cmnts by points
+  //insert into google-spreadsheet
+  // await browser.close();
 })();
