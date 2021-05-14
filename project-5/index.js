6c788e1342dd40de1168db5555521935480bb58d
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-14 11:59:23 +0000

    select all cmnts and scrape text+point

--- project-5/index.js
+++ project-5/index.js
@@ -2,21 +2,37 @@ const puppeteer = require("puppeteer");
 const url =
   "https://old.reddit.com/r/learnprogramming/comments/4q6tae/i_highly_recommend_harvards_free_online_2016_cs50/";
 (async function () {
-  //boiler plate
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.goto(url);
-  //expand all comment threads
+  // expand all comment threads
   let expandsBtns = await page.$$(".morecomments");
   while (expandsBtns.length) {
     for (let button of expandsBtns) {
       await button.click();
       await page.waitForTimeout(300);
     }
+    await page.waitForTimeout(500);
     expandsBtns = await page.$$(".morecomments");
   }
 
   //select all cmnts and scrape text+point
+  const entryCmnts = await page.$$(".entry");
+  for (let entry of entryCmnts) {
+    //points
+    const points = await entry
+      .$eval(".score", (el) => el.textContent)
+      .catch((err) => {});
+    const formattedCmnts = [];
+    const rawComment = await entry
+      .$eval(".usertext-body", (el) => el.textContent)
+      .catch((err) => {});
+    if (points && rawComment) {
+      //filter comments
+      const comment = rawComment.replace(/\n/g, "");
+      formattedCmnts.push({points, comment});
+    }
+  }
   //sort cmnts by points
   //insert into google-spreadsheet
   // await browser.close();
