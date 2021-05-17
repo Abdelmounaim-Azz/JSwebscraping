fb35e74b8129a78885e11af65a7567a9b677cb51
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-17 02:48:43 +0100

    type username+password

--- project-6/index.js
+++ project-6/index.js
@@ -4,5 +4,8 @@ const puppeteer = require("puppeteer");
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.goto("https://instagram.com");
+  const inputs = await page.$$(".input");
+  await inputs[0].type(process.env.USERNAME);
+  await inputs[1].type(process.env.PASSWORD);
   // await browser.close();
 })();
