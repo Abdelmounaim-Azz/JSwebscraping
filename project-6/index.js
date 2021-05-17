fcbceba39e7617db04654f192f1433e3532aca11
Abdelmounaim-Azz <abdelmounaim.azziza@outlook.fr>
2021-05-17 03:04:57 +0100

    load env vars

--- project-6/index.js
+++ project-6/index.js
@@ -1,12 +1,13 @@
 const puppeteer = require("puppeteer");
 require("dotenv").config();
+const USERNAME = "azz.sahafrica";
 (async function () {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.goto("https://instagram.com");
   await page.waitForSelector("input");
   const inputs = await page.$$("input");
-  await inputs[0].type(process.env.USERNAME);
+  await inputs[0].type(USERNAME);
   await inputs[1].type(process.env.PASSWORD);
   // await browser.close();
 })();
